0b34cf1188e4d02c3021b0407b6697c30ba3d96e
Vincent Berthiaume <vberthiaume@gmail.com>
2016-03-04 11:50:30 -0500

    trying and failing to understand this shit

--- udacity/1-Softmax.py
+++ udacity/1-Softmax.py
@@ -10,12 +10,13 @@ def softmax(x):
     sum_rows = np.sum(x,0) 
     return x / sum_rows
 
-print(softmax(scores))
+#print(softmax(scores))
 
 # Plot softmax curves
 import matplotlib.pyplot as plt
 x = np.arange(-2.0, 6.0, 0.1)
 scores = np.vstack([x, np.ones_like(x), 0.2 * np.ones_like(x)])
-
-plt.plot(x, softmax(scores).T, linewidth=2)
+#plt.plot(x, softmax(scores).T, linewidth=2)
+plt.plot(softmax(scores).T)
+#print(softmax(scores))
 plt.show()
--- udacity/test.py
+++ udacity/test.py
@@ -1,12 +1,16 @@
 import numpy as np
 
-A = np.array([[1.,0.,0.],[1.,1.,1.],[1.,2.,2.],[1.,2.,3.]])	#array with 4 rows, 3 colums
-A = A.T						#transpose array, so it becomes 3 rows, 4 cols
-sum_rows = np.sum(A,0)				#vector containing the sum of rows
+#x = np.array([[1.,0.,0.],[1.,1.,1.],[1.,2.,2.],[1.,3.,3.],[1.,4.,4.]])	#array with 4 rows, 3 colums
+#x = x.T						#transpose array, so it becomes 3 rows, 4 cols
 
-#print (np.divide(A,sum_rows[np.newaxis,:]))
+x = np.arange(-2.0, 6.0, 0.1)
+x = np.vstack([x, np.ones_like(x), 0.2 * np.ones_like(x)])
 
-print (A /sum_rows)
+sum_rows = np.sum(x,0)				#vector containing the sum of rows
+
+#print (np.divide(x,sum_rows[np.newaxis,:]))
+
+print (x /sum_rows)
 
 
 
