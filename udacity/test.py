2831451643df12aba5d62d21c687226e26fd4cfc
Vincent Berthiaume <vberthiaume@gmail.com>
2016-03-04 11:10:58 -0500

    test.py should implement softmax

--- udacity/test.py
+++ udacity/test.py
@@ -1,8 +1,13 @@
 import numpy as np
 
-A = np.array([[1,2,3],[2,3,4],[3,4,5]])
-A = A.T
-print (A)
+A = np.array([[1.,0.,0.],[1.,1.,1.],[1.,2.,2.],[1.,2.,3.]])	#array with 4 rows, 3 colums
+A = A.T						#transpose array, so it becomes 3 rows, 4 cols
+sum_rows = np.sum(A,0)				#vector containing the sum of rows
+
+#print (np.divide(A,sum_rows[np.newaxis,:]))
+
+print (A /sum_rows)
+
 
 
 
