e91c832358c35336f0f5a1a20da5f1f25196c335
Vincent Berthiaume <vberthiaume@gmail.com>
2016-03-10 09:08:58 -0500

    refactoring and commenting

--- udacity/1-Softmax.py
+++ udacity/1-Softmax.py
@@ -1,18 +1,22 @@
 """Softmax."""
-
-scores = [3.0, 1.0, 0.2]
-
 import numpy as np
+import matplotlib.pyplot as plt
 
 def softmax(x):
     """Compute softmax values for each sets of scores in x."""
-    #for each column, find the sum of all elements, and divide each row element by its column sum
+    #sum e^x for all x, accross rows (dimention 0). this gives a vector with as many elements as x has columns
     sum_rows = np.sum(np.exp(x),0) 
+    #return the normalization of e^x 
     return np.exp(x) / sum_rows
 
-# Plot softmax curves
-import matplotlib.pyplot as plt
+#x is a vector that ranges from -2 to 6 with increments of .1
 x = np.arange(-2.0, 6.0, 0.1)
+
+#vstack stacks its arguments as rows, so here scores will have 3 rows. 
+#And ones_like produces an array of the same form as its argument, but filled with ones.
+#so first row is x, second is a row of 1s, third is a row of .2s
 scores = np.vstack([x, np.ones_like(x), 0.2 * np.ones_like(x)])
+
+# Plot softmax curves. X axis is x, the arange. Y axis is the softmax values for the scores
 plt.plot(x, softmax(scores).T, linewidth=2)
 plt.show()
