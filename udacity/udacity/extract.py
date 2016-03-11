d122b07d646b00519831315295d41e9ed7a86415
Vincent Berthiaume <vberthiaume@gmail.com>
2016-03-11 10:17:17 -0500

    jupyter notebook for ass1 keeps crashing when trying to extract data from the datasets, so copied relevant code in extract.py and finally got it to work.

--- udacity/udacity/extract.py
+++ udacity/udacity/extract.py
@@ -1,11 +1,36 @@
-import os
+# These are all the modules we'll be using later. Make sure you can import them
+# before proceeding further.
+from __future__ import print_function
+print('-------------- START IMPORT --------------')
+import matplotlib.pyplot as plt
 import numpy as np
+import os
 import sys
 import tarfile
+from IPython.display import display, Image
+from scipy import ndimage
+from sklearn.linear_model import LogisticRegression
+from six.moves.urllib.request import urlretrieve
+from six.moves import cPickle as pickle
+print('------------ START DOWNLOAD --------------')
+url = 'http://yaroslavvb.com/upload/notMNIST/'
 
-train_filename = 'notMNIST_large.tar.gz'
-test_filename = 'notMNIST_small.tar.gz'
+def maybe_download(filename, expected_bytes, force=False):
+  """Download a file if not present, and make sure it's the right size."""
+  if force or not os.path.exists(filename):
+    filename, _ = urlretrieve(url + filename, filename)
+  statinfo = os.stat(filename)
+  if statinfo.st_size == expected_bytes:
+    print('Found and verified', filename)
+  else:
+    raise Exception(
+      'Failed to verify ' + filename + '. Can you get to it with a browser?')
+  return filename
 
+train_filename = maybe_download('notMNIST_large.tar.gz', 247336696)
+test_filename = maybe_download('notMNIST_small.tar.gz', 8458043)
+
+print('------------ START EXTRACT --------------')
 num_classes = 10
 np.random.seed(133)
 
@@ -32,3 +57,5 @@ def maybe_extract(filename, force=False):
   
 train_folders = maybe_extract(train_filename)
 test_folders = maybe_extract(test_filename)
+
+print('------------ END EXTRACT --------------')
