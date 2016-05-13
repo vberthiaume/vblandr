d5382d517b3780a24019b17939c863ae2e077388
Vincent Berthiaume <vberthiaume@gmail.com>
2016-05-13 13:29:21 -0400

    exited pycharm (which saved some project file?) and counting files

--- src/count_audio_files.py
+++ src/count_audio_files.py
@@ -1,1 +1,6 @@
- 
+import os, os.path
+
+# path joining version for other paths
+music_dir = '/media/kxstudio/Wisdom/music/From Parts Unknown'
+file_cnt = len([name for name in os.listdir(music_dir) if os.path.isfile(os.path.join(music_dir, name))])
+print "folder", music_dir, "contains", file_cnt, "files."
