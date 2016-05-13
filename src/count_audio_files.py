c704be45c315075f1d7fc4ed205dcb3253d21190
Vincent Berthiaume <vberthiaume@gmail.com>
2016-05-13 13:40:38 -0400

    added recursive funct

--- src/count_audio_files.py
+++ src/count_audio_files.py
@@ -1,9 +1,14 @@
 import os, os.path
 
-
 def count_files( dir ):
 	return len([name for name in os.listdir(dir) if os.path.isfile(os.path.join(dir, name))])
 
-music_dir = '/media/kxstudio/Wisdom/music/From Parts Unknown'
+def count_files_rec(dir):
+	"this will count recursively in sub folders"
+	path, dirs, files = os.walk("/usr/lib").next()
+	return len(files)
 
-print "folder", music_dir, "contains", count_files(music_dir), "files."
+# music_dir = '/media/kxstudio/Wisdom/music/From Parts Unknown'
+music_dir = '/media/kxstudio/Wisdom/music/Audiobooks'
+
+print "folder", music_dir, "contains", count_files_rec(music_dir), "files."
