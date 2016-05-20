8c921ca0bbed310d08b814c951e81a463d502947
Vincent Berthiaume <vberthiaume@gmail.com>
2016-05-20 09:54:00 -0400

    counting wav and mp3 files

--- src/count_audio_files.py
+++ src/count_audio_files.py
@@ -24,17 +24,26 @@ def count_files_rec(dir):
 	#lstNumberOfFilesContainedInEachFolder = [len(files) for path, dirs, files in os.walk(dir)]
 	#return sum(lstNumberOfFilesContainedInEachFolder)
 
-	#files is a 2 layer list with cells containing the file names in each dirs folder
+	#os.walk will go through each folder in dir (including dir), and for each folder will
+	#	* set path to current path
+	#	* set dirs to a list of directories that the current path contains
+	#	* set files to a list of files that the current path contains
+	totalNumberOfAudioFiles = 0
 	for path, dirs, files in os.walk(dir):
-		print path
-		print dirs
-		print files
-		print "=================="
+		# print path
+		# print dirs
+		# print files
+		for file in files:
+			if not file.startswith('.') and (file.endswith('.wav') or file.endswith('.mp3')):
+				# print file
+				totalNumberOfAudioFiles += 1
+		# print "=================="
+	return totalNumberOfAudioFiles
 
 	
 
 music_dir = '/media/kxstudio/Wisdom/music/Audiobooks'
-count_files_rec(music_dir)
+print "total audio files = ", count_files_rec(music_dir)
 #print "folder", music_dir, "contains", count_files_rec(music_dir), "files."
 
 
