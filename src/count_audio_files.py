870388f035585c0a23201a88e101b5e4b8fffd0b
Vincent Berthiaume <vberthiaume@gmail.com>
2016-05-27 13:29:33 -0400

    changed all tabs to spaces goddammit

--- src/count_audio_files.py
+++ src/count_audio_files.py
@@ -5,22 +5,31 @@ import sys
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../sms-tools/software/models/'))
 import utilFunctions as UF
 
-def find_audio_files(dir):
-	"return the path of all audio files (wav and mp3) in dir and its subfolders"
-	#os.walk will go through each folder in dir (including dir), and for each folder will
-	#	* set path to current path
-	#	* set dirs to a list of directories that the current path contains
-	#	* set files to a list of files that the current path contains
-	allAudioFiles = []
-	for path, dirs, files in os.walk(dir):
-		for file in files:
-			if not file.startswith('.') and (file.endswith('.wav') or file.endswith('.mp3')):
-				allAudioFiles.append(path+"/"+file)
-	return allAudioFiles
+def find_audio_genres_and_files(dir):
+    "return the path of all audio files (wav and mp3) in dir and its subfolders"
+    #os.walk will go through each folder in dir (including dir), and for each folder will
+    #   * set path to current path
+    #   * set dirs to a list of directories that the current path contains
+    #   * set files to a list of files that the current path contains
+    allAudioGenres = []
+    allAudioFiles = []
+    in_top_dir = True
+    for path, dirs, files in os.walk(dir):
+        if in_top_dir:
+            allAudioGenres = list(dirs)
+            in_top_dir = False
+        for file in files:
+            if not file.startswith('.') and (file.endswith('.wav') or file.endswith('.mp3')):
+                allAudioFiles.append(path+"/"+file)
+    return (allAudioGenres, allAudioFiles)
 
-	
+# for now using .wavs, but most of my collection is in mp3, so need to figure how to use mp3s.
+
+# I need to build a training set. First thing is to match input patterns with target patterns (labels).
+# let's say the first folder is the label, and all audio files in sub dirs are
+    a
 music_dir = '/media/kxstudio/Wisdom/music/Audiobooks'
-allAudioFiles = find_audio_files(music_dir)
+allAudioGenres, allAudioFiles = find_audio_genres_and_files(music_dir)
 print "total audio files = ", len(allAudioFiles)
 print allAudioFiles[1]
 (fs, x) = UF.wavread(allAudioFiles[1])
