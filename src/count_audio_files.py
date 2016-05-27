145835aebc78efde0ae23bfeb9354311106f7f9e
Vincent Berthiaume <vberthiaume@gmail.com>
2016-05-27 12:31:28 -0400

    .gitignore

--- src/count_audio_files.py
+++ src/count_audio_files.py
@@ -5,8 +5,6 @@ import sys
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../sms-tools/software/models/'))
 import utilFunctions as UF
 
-
-
 def find_audio_files(dir):
 	"return the path of all audio files (wav and mp3) in dir and its subfolders"
 	#os.walk will go through each folder in dir (including dir), and for each folder will
@@ -21,7 +19,6 @@ def find_audio_files(dir):
 	return allAudioFiles
 
 	
-
 music_dir = '/media/kxstudio/Wisdom/music/Audiobooks'
 allAudioFiles = find_audio_files(music_dir)
 print "total audio files = ", len(allAudioFiles)
