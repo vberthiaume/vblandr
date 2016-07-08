8c05f73f1e59563d5ed29fe481bd5909ecf6491a
Vincent Berthiaume <vberthiaume@gmail.com>
2016-07-07 20:06:45 -0400

    added DataSet class

--- src/mainMnist.py
+++ src/mainMnist.py
@@ -32,7 +32,8 @@ from six.moves import xrange  # pylint: disable=redefined-builtin
 from six.moves import cPickle as pickle
 #tensorflow stuff
 import tensorflow as tf
- from tensorflow.contrib.learn.python.learn.datasets.mnist import read_data_sets
+# from tensorflow.contrib.learn.python.learn.datasets.mnist import read_data_sets
+from tensorflow.python.framework import dtypes
 #sms-tools stuff
 import sys, os, os.path
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../sms-tools/software/models/'))
@@ -63,7 +64,7 @@ def main(_):
 def run_training():
     """Train MNIST for a number of steps."""
     # Get the sets of images and labels for training, validation, and test on MNIST.
-    data_sets = read_data_sets(FLAGS.train_dir, FLAGS.fake_data)
+    data_sets = read_data_sets(FLAGS.train_dir)
 
     # Tell TensorFlow that the model will be built into the default Graph.
     with tf.Graph().as_default():
@@ -123,6 +124,15 @@ def run_training():
                 # Evaluate against the test set.
                 print('Test Data Eval:')
                 do_eval(sess, eval_correct, images_placeholder, labels_placeholder, data_sets.test)
+
+def read_data_sets(train_dir, dtype=dtypes.float32):
+    train       = DataSet(train_images, train_labels, dtype=dtype)
+    validation  = DataSet(validation_images, validation_labels, dtype=dtype)
+    test        = DataSet(test_images, test_labels, dtype=dtype)
+
+    return base.Datasets(train=train, validation=validation, test=test)
+
+
 
 def placeholder_inputs(batch_size):
     """Generate placeholder variables to represent the input tensors.
@@ -248,8 +258,76 @@ def evaluation(logits, labels):
     # Return the number of true entries.
     return tf.reduce_sum(tf.cast(correct, tf.int32))
 
+class DataSet(object):
+    def __init__(self, images, labels, dtype=dtypes.float32):
+        """Construct a DataSet. `dtype` can be either `uint8` to leave the input as `[0, 255]`, or `float32` to rescale into `[0, 1]`."""
+        dtype = dtypes.as_dtype(dtype).base_dtype
+        if dtype not in (dtypes.uint8, dtypes.float32):
+            raise TypeError('Invalid image dtype %r, expected uint8 or float32' %
+                          dtype)
+        if fake_data:
+            self._num_examples = 10000
+            self.one_hot = one_hot
+        else:
+            assert images.shape[0] == labels.shape[0], ('images.shape: %s labels.shape: %s' % (images.shape, labels.shape))
+            self._num_examples = images.shape[0]
 
+            # Convert shape from [num examples, rows, columns, depth] to [num examples, rows*columns] (assuming depth == 1)
+            assert images.shape[3] == 1
+            images = images.reshape(images.shape[0], images.shape[1] * images.shape[2])
+            if dtype == dtypes.float32:
+                # Convert from [0, 255] -> [0.0, 1.0].
+                images = images.astype(numpy.float32)
+                images = numpy.multiply(images, 1.0 / 255.0)
+            self._images = images
+            self._labels = labels
+            self._epochs_completed = 0
+            self._index_in_epoch = 0
 
+    @property
+    def images(self):
+        return self._images
+
+    @property
+    def labels(self):
+        return self._labels
+
+    @property
+    def num_examples(self):
+        return self._num_examples
+
+    @property
+    def epochs_completed(self):
+        return self._epochs_completed
+
+    def next_batch(self, batch_size, fake_data=False):
+        """Return the next `batch_size` examples from this data set."""
+        if fake_data:
+            fake_image = [1] * 784
+            if self.one_hot:
+                fake_label = [1] + [0] * 9
+            else:
+                fake_label = 0
+            return [fake_image for _ in xrange(batch_size)], [
+                fake_label for _ in xrange(batch_size)
+            ]
+        start = self._index_in_epoch
+        self._index_in_epoch += batch_size
+        if self._index_in_epoch > self._num_examples:
+            # Finished epoch
+            self._epochs_completed += 1
+            # Shuffle the data
+            perm = numpy.arange(self._num_examples)
+            numpy.random.shuffle(perm)
+            self._images = self._images[perm]
+            self._labels = self._labels[perm]
+            # Start next epoch
+            start = 0
+            self._index_in_epoch = batch_size
+            assert batch_size <= self._num_examples
+        end = self._index_in_epoch
+        return self._images[start:end], self._labels[start:end]
+    # ENDOF DataSet 
 
 if __name__ == '__main__':
     tf.app.run()
