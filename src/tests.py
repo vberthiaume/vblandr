58305814067118bf9b59f3f8bb9a7cae6c06214e
Vincent Berthiaume <vberthiaume@gmail.com>
2016-07-11 11:58:58 -0400

    tests with double pickling working

--- src/tests.py
+++ src/tests.py
@@ -20,23 +20,47 @@ import numpy as np
 
 def main():
     
-    song_filename = '/media/kxstudio/LUSSIER/music/train_small/metal/01-architects-early_grave.mp3'
-
-    pickle_filename = song_filename + '.pickle'
-
-    song_pcm = songFile2pcm(song_filename)
     SAMPLE_COUNT = 10 * 44100   # first 10 secs of audio
-    song_pcm = song_pcm[0:SAMPLE_COUNT]     
 
+    song1_filename = '/media/kxstudio/LUSSIER/music/train_small/metal/01-architects-early_grave.mp3'
+    pickle1_filename = song1_filename + '.pickle'
+    song1_pcm = songFile2pcm(song1_filename)
+    song1_pcm = song1_pcm[0:SAMPLE_COUNT] 
     #maybe_pickle
-    with open(pickle_filename, 'wb') as f:
-        pickle.dump(song_pcm, f, pickle.HIGHEST_PROTOCOL)
+    with open(pickle1_filename, 'wb') as f1:
+        pickle.dump(song1_pcm, f1, pickle.HIGHEST_PROTOCOL)
 
+    # with open(pickle1_filename, 'rb') as f3:
+    #     unpickled1_song_pcm = pickle.load(f3)       
+    # write_test_wav(unpickled1_song_pcm, song1_filename + '.wav')
+
+    song2_filename = '/media/kxstudio/LUSSIER/music/train_small/metal/01. The Bitter End.mp3'
+    pickle2_filename = song2_filename + '.pickle'
+    song2_pcm = songFile2pcm(song2_filename)
+    song2_pcm = song2_pcm[0:SAMPLE_COUNT] 
+    #maybe_pickle
+    with open(pickle2_filename, 'wb') as f2:
+        pickle.dump(song2_pcm, f2, pickle.HIGHEST_PROTOCOL)
     
-    with open(pickle_filename, 'rb') as f:
-        unpickled_song_pcm = pickle.load(f)
-        
-    write_test_wav(unpickled_song_pcm, song_filename + '.wav')
+
+
+    all_pickle_files = [pickle1_filename, pickle2_filename]
+    
+    all_pickle_filename = '/media/kxstudio/LUSSIER/music/train_small/metal/all.pickle'
+    
+    #builddatasets
+    f = open(all_pickle_filename, 'wb')
+    save = {'all_pickle_files':    all_pickle_files}
+    pickle.dump(save, f, pickle.HIGHEST_PROTOCOL)
+    f.close()
+
+    with open(all_pickle_filename, 'rb') as f:
+        save = pickle.load(f)
+        unpickled_all_pickle_files   = save['all_pickle_files']
+
+        for id, cur_pickled_filename in enumerate (unpickled_all_pickle_files):
+            cur_unpickled_song_pcm = pickle.load( open( cur_pickled_filename, "rb" ) )
+            write_test_wav(cur_unpickled_song_pcm, '/media/kxstudio/LUSSIER/music/train_small/metal/' + str(id) + '.wav')
 
 
 
