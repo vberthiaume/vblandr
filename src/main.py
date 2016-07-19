1d6128e7847a196c88e5a0625cc84bac014f01a9
Vincent Berthiaume <vberthiaume@gmail.com>
2016-07-19 16:34:09 -0400

    using very small training, no testing

--- src/main.py
+++ src/main.py
@@ -145,12 +145,9 @@ def read_data_sets(train_dir, dtype=dtypes.float32):
 
     #build train, valid and test datasets
     global overall_song_id
-    overall_song_id = 0
+
     pickle_file = buildDataSets()
 
-    #train_images should be a 4D uint8 numpy array [index, y, x, depth]."""
-
-    #with is just a safe way of dealing with resources. handles correct closing if exceptions, etc
     with open(pickle_file, 'rb') as f:
         save = pickle.load(f)
         train_dataset   = save['wholeTrainDataset']
@@ -164,7 +161,6 @@ def read_data_sets(train_dir, dtype=dtypes.float32):
         print('after pickling, Validation set', valid_dataset.shape, valid_labels.shape)
         print('after pickling, Test set',       test_dataset.shape,  test_labels.shape)
 
-        #TODO: train_dataset ETC MIGHT STILL BE PICKLED AT THAT POINT, EXPLAINING WHY AUDIO IS GARBLED? 
 
     train       = DataSet(train_dataset, train_labels,  dtype=dtype)
     validation  = DataSet(valid_dataset, valid_labels,  dtype=dtype)
@@ -254,19 +250,19 @@ class DataSet(object):
 
 def buildDataSets():
 
-    trainGenreNames, trainGenrePaths = listGenres(LIBRARY_PATH + 'train_small/')
-    testGenreNames, testGenrePaths   = listGenres(LIBRARY_PATH + 'test_small/')
+    trainGenreNames, trainGenrePaths = listGenres(LIBRARY_PATH + 'train_very_small/')#'train_small/')
+    # testGenreNames, testGenrePaths   = listGenres(LIBRARY_PATH + 'test_small/')
     pickle_file =                                 LIBRARY_PATH + 'allData.pickle'
         
     allPickledTrainFilenames = maybe_pickle(trainGenrePaths, FORCE_PICKLING)
-    allPickledTestFilenames  = maybe_pickle(testGenrePaths, FORCE_PICKLING)
+    # allPickledTestFilenames  = maybe_pickle(testGenrePaths, FORCE_PICKLING)
     # all_train_data_sets = load_all_genres(trainGenrePaths, FORCE_PICKLING)
     # all_test_data_sets  = load_all_genres(testGenrePaths, FORCE_PICKLING)
 
 
     #call merge_dataset on data_sets and labels
-    wholeValidDataset, wholeValidLabels, wholeTrainDataset, wholeTrainLabels = merge_dataset(allPickledTrainFilenames, s_iTrainSize, s_iValid_size)
-    _,                                _, wholeTestDataset,  wholeTestLabels  = merge_dataset(allPickledTestFilenames,  s_iTestSize)
+    wholeValidDataset, wholeValidLabels, wholeTrainDataset, wholeTrainLabels = merge_dataset(allPickledTrainFilenames, s_iTrainSize, 0)#s_iValid_size)
+    # _,                                _, wholeTestDataset,  wholeTestLabels  = merge_dataset(allPickledTestFilenames,  s_iTestSize)
 
     #PUT BACK RANDOM
     # wholeTrainDataset, wholeTrainLabels = randomize(wholeTrainDataset, wholeTrainLabels)
