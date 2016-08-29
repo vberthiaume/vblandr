85c5bedeffdb0486912a8180ebe30cf8b85d10e9
Vincent Berthiaume <vberthiaume@gmail.com>
2016-08-29 13:23:49 -0400

    added my copyright. really not sure if this is the way to do it...!

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+# All edits to original document Copyright 2016 Vincent Berthiaume. 
 # Copyright 2015 The TensorFlow Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
