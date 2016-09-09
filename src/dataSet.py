a2def23a2358fb138cd2425cdb795a8ede2245a7
Vincent Berthiaume <vberthiaume@gmail.com>
2016-09-09 16:13:15 -0400

    asdf

--- src/dataSet.py
+++ src/dataSet.py
@@ -69,9 +69,9 @@ def getAllDataSets(train_dir, dtype=dtypes.float32):
 
 def dense_to_one_hot(labels_dense, num_classes):
     """Convert class labels from scalars to one-hot vectors."""
-    num_labels = labels_dense.shape[0]
-    index_offset = numpy.arange(num_labels) * num_classes
-    labels_one_hot = numpy.zeros((num_labels, num_classes))
+    num_labels      = labels_dense.shape[0]
+    index_offset    = np.arange(num_labels) * num_classes
+    labels_one_hot  = np.zeros((num_labels, num_classes))
     labels_one_hot.flat[index_offset + labels_dense.ravel()] = 1
     return labels_one_hot
 
