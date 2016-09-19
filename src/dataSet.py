e55a9756dd364c58b96901601891d830d1eee048
Vincent Berthiaume <vberthiaume@gmail.com>
2016-09-19 11:06:26 -0400

    notes and license

--- src/dataSet.py
+++ src/dataSet.py
@@ -1,3 +1,21 @@
+# All edits to original document Copyright 2016 Vincent Berthiaume. 
+#
+# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+
 import tensorflow as tf
 from tensorflow.python.framework import dtypes
 
@@ -27,7 +45,7 @@ s_iTestSize     = 6 * NUM_CLASSES  # 10000
 
 SAMPLE_COUNT = 10 * 44100   # first 10 secs of audio
 TOTAL_INPUTS = SAMPLE_COUNT
-FORCE_PICKLING = False
+FORCE_PICKLING = True
 Datasets = collections.namedtuple('Datasets', ['train', 'validation', 'test'])
 overall_song_id = 0
 ONE_HOT = False
@@ -271,7 +289,7 @@ def songFile2pcm(song_path):
     pipe = sp.Popen(command, stdout=sp.PIPE)
     #read the output into a numpy array
     stdoutdata = pipe.stdout.read()
-    audio_array = np.fromstring(stdoutdata, dtype="int16")
+    audio_array = np.fromstring(stdoutdata, dtype=np.int16)
 
     # size = len(audio_array)
     # print ("size: ", size)
