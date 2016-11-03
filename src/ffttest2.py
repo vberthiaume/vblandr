ac197c271c3b2096c4449ed7011ec0657a479021
Vincent Berthiaume <vberthiaume@gmail.com>
2016-11-03 16:42:06 -0400

    fft tests

--- src/ffttest2.py
+++ src/ffttest2.py
@@ -1,23 +1,22 @@
 import numpy as np
 
 #SPLIT AND RECONSTRUCT FFT OUTPUT
-x = np.array([5., 4., 3., 2., 1., 1., 2., 3., 4., 5.])
-half = len(x)/2
-x = x[:half]
-x = np.append(x, np.zeros(len(x)))
-for i in np.arange(half):
-    # x[half+i] = x[half-i-1]
-    x[i] = x[len(x)-i-1]
-
-print x
-
-
-# # SPLIT AND RECONSTRUCT FFT OUTPUT, REVERSING THE DROPPED HALF TO MAKE IT LOOK SIMMETRICAL
 # x = np.array([5., 4., 3., 2., 1., 1., 2., 3., 4., 5.])
 # half = len(x)/2
 # x = x[:half]
-# x = np.insert(x, np.zeros(len(x)), 0)
+# x = np.append(x, np.zeros(len(x)))
 # for i in np.arange(half):
-#     x[i] = x[len(x)-i-1]
+#     x[half+i] = x[half-i-1]
 
 # print x
+
+
+# SPLIT AND RECONSTRUCT FFT OUTPUT, REVERSING THE DROPPED HALF TO MAKE IT LOOK SIMMETRICAL
+x = np.array([5., 4., 3., 2., 1., 1., 2., 3., 4., 5.])
+half = len(x)/2
+x = x[:half]
+x = np.insert(x, np.zeros(len(x)), 0)
+for i in np.arange(half):
+    x[i] = x[len(x)-i-1]
+
+print x
