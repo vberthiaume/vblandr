6fcb1f8275c9931133862a328d1e4037e25e3c25
Vincent Berthiaume <vberthiaume@gmail.com>
2016-11-10 13:08:47 -0500

    refactor

--- src/fftIfftTests.py
+++ src/fftIfftTests.py
@@ -20,23 +20,24 @@ pipe = sp.Popen(command, stdout=sp.PIPE)
 #read the output into a numpy array
 stdoutdata = pipe.stdout.read() 
 audio_array = np.fromstring(stdoutdata, dtype=np.int16)
+audio_array = audio_array[:2**16]
 #--------------------------------------------------------------
 
 
 
+#---- FFT THEN IFFT ------------------------
+fft_output  = fft (audio_array)
+ifft_output = ifft(fft_output).real
 
-audio_array = audio_array[:2**16]
-#
-#fft_output  = fft (audio_array)
-#ifft_output = ifft(fft_output)
-#
-##print (fft_output)
-#
-##SAVE WAVE AS NEW FILE
-#filename = song_path = '/home/gris/Music/vblandr/testIfft.wav'
-#scikits.audiolab.wavwrite(ifft_output, filename, fs=44100, enc='pcm16')
+#this stuff is equivalent
+#fft_output  = np.fft.rfft (audio_array, axis=0)
+#print "fft_output is type", type(fft_output[0])
+#ifft_output = np.fft.irfft(fft_output,  axis=0)
+#print "ifft_output is type", type(ifft_output[0])
 
-fft_output  = np.fft.rfft (audio_array,  axis=0)
-ifft_output = np.fft.irfft(fft_output, axis=0)
+
+
+#--SAVE WAVE AS NEW FILE ----------------
 ifft_output = np.round(ifft_output).astype('int16')
 wavfile.write('/home/gris/Music/vblandr/testIfft.wav', 44100, ifft_output)
+#scikits.audiolab.wavwrite(ifft_output, '/home/gris/Music/vblandr/testIfft.wav', fs=44100, enc='pcm16')
