4018cdff8629410c698c78712dcd90748e478056
Vincent Berthiaume <vberthiaume@gmail.com>
2016-12-12 15:08:12 -0500

    added extra plots and measures in silence test. Also extra normalizing.

--- src/silenceTest.py
+++ src/silenceTest.py
@@ -5,11 +5,15 @@ from scipy.fftpack import fft, ifft
 from scipy.io import wavfile
 import bisect
 import matplotlib.pyplot as plt
+import time
+
+plt.rcParams['agg.path.chunksize'] = 10000
 
 #--CONVERT MP3 TO WAV------------------------------------------
 #song_path = '/home/gris/Music/vblandr/test_small/punk/07 Alkaline Trio - Only Love.mp3'
 #song_path = '/mnt/c/Users/barth/Documents/vblandr/train_small/punk/01 - True North.mp3'
-song_path = '/mnt/c/Users/barth/Documents/vblandr/train_small/audiobook/Blaise_Pascal_-_Discours_sur_les_passions_de_l_amour.mp3'
+#song_path = '/mnt/c/Users/barth/Documents/vblandr/train_small/audiobook/Blaise_Pascal_-_Discours_sur_les_passions_de_l_amour.mp3'
+song_path = '/home/gris/Music/vblandr/train_small/audiobook/Blaise_Pascal_-_Discours_sur_les_passions_de_l_amour.mp3'
 command = [ 'ffmpeg',
         '-i', song_path,
         '-f', 's16le',
@@ -26,12 +30,39 @@ audio_array = np.fromstring(stdoutdata, dtype=np.int16)
 #--------------------------------------------------------------
 
 def removeInitialSilence(cur_song_pcm):
+    
+    #start_time = time.clock()
+
+    #raw
+    #min = np.min(cur_song_pcm)
+    #max = np.max(cur_song_pcm)
+    #print ("raw min:", min, "max:", max)
+    
     #using absolute value
     env = abs(cur_song_pcm)
+    #min = np.min(env)
+    #max = np.max(env)
+    #print ("abs min:", min, "max:", max)
+ 
+    #float
     env = env.astype(np.float32)            #cast the array into float32
-    env = np.multiply(env, 1.0 / 65536)     #convert int16 range into [-.5, .5], but really because of the abs we're already between [0,.5]
-    env = np.multiply(env, 2)               #convert [0,.5] range into [0,1.0] 
+    #min = np.min(env)
+    #max = np.max(env)
+    #print ("float min:", min, "max:", max)
+ 
+    #norm1
+    max = np.max(env)
+    env = np.multiply(env, 1.0 / max)     #convert int16 range into [-.5, .5], but really because of the abs we're already between [0,.5]
+    min = np.min(env)
+    max = np.max(env)
+    print ("norm1 min:", min, "max:", max)
+ 
+    #end_time = time.clock()
+    #print ("time:", end_time - start_time)
     
+    mediaplt.plot(env)
+    plt.show()
+
     #convolving as a way to do a fast moving average
     N = 100
     env = np.convolve(env, np.ones((N,))/N)[(N-1):]
@@ -40,21 +71,22 @@ def removeInitialSilence(cur_song_pcm):
     #print np.max(env[:44100])
     
     #at 1.5s, we're clearly into audio, what is the max amplitude?
-    print "before .5s, max: ", np.max(env[:.5*44100])
+    #print "before .5s, max: ", np.max(env[:.5*44100])
     
     #here we're still in noise part
-    print "in vocal part, max: ", np.max(env[.625*44100])
+    #print "in vocal part, max: ", np.max(env[.625*44100])
     
-    #these don't work on hesse
-    #plt.plot(env)
-    #plt.show()
-
     #detect first non-silent sample
     threshold = .00004
     
     endOfSilence = bisect.bisect(env,threshold)
     
     print "end of silence: ", endOfSilence
+
+    #these don't work on hesse
+    plt.plot(env)
+    plt.show()
+
     return cur_song_pcm[endOfSilence:]
 
 #---- REMOVE SILENCE --------------------
@@ -64,8 +96,8 @@ ifft_output = ifft_output[:1*44100]
 
 #--SAVE WAVE AS NEW FILE ----------------
 ifft_output = np.round(ifft_output).astype('int16')
-#wavfile.write('/home/gris/Music/vblandr/testIfft.wav', 44100, ifft_output)
-wavfile.write('/mnt/c/Users/barth/Documents/vblandr/silenceTest.wav', 44100, ifft_output)
+wavfile.write('/home/gris/Music/vblandr/silenceTest.wav', 44100, ifft_output)
+#wavfile.write('/mnt/c/Users/barth/Documents/vblandr/silenceTest.wav', 44100, ifft_output)
 
 
 
